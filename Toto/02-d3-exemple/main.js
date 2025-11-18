70e972cf710c05a36432e93a9ed7d4dfe5777ad1
RatrackSnowdozer <rsnowdozer@gmail.com>
2025-11-18 14:36:17 +0000

    ok

--- Toto/02-d3-exemple/main.js
+++ Toto/02-d3-exemple/main.js
@@ -1,10 +1,11 @@
 console.log("Hello Ratrack !");
 
 const scv=document.querySelector("svg");
+SvgNS="http://www.w3.org/2000/svg""
 
 for (let i=0; i=i++) {
     console.log('iteration number : ${i}');
-    const rect=document.createElementNS("http://www.w3.org/2000/svg");
+    const rect=document.createElementNS(SvgNS,"rect");
     rect.setAttribute("x",10);
     rect.setAttribute("y",10+i*(height+Gap));
     rect.setAttribute("width",600);
